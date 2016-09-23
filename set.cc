e5f476810e0710e68b10bcd1e9146206761888e2
zezeon <zezeon@msn.com>
2016-09-23 14:22:52 +0900

    palidic

--- set.cc
+++ set.cc
@@ -6,6 +6,7 @@ template <typename T, int N> class Set
 public:
 	bool on[N] {};//true일 때만 그에 대응하는 배열의 요소가 유효.
 	T arr[N];//데이터를 담을 배열
+
 	void insert(T n) {
 		if(!is_in(n)) {
 			for(int i=0; i<N; i++) {
@@ -52,17 +53,38 @@ public:
 		return s;
 	}
 
+	friend ostream& operator<<(ostream& o, const Set<T, N>& r) {// const 있어야 함.
+		o << '{';
+		for(int i=0; i<N; i++) if(r.on[i]) o << r.arr[i] << ',';
+		o << "\b }";
+		return o;
+	}
+
+	Set<T, N> operator!() {
+		Set<T, N> s;
+		for(int i=0; i<N; i++) s.on[i] = !on[i];
+		return s;
+	}
+
+	Set<T, N> operator&&(const Set<T, N>& r) {
+		Set<T, N> s;
+		for(int i=0; i<N; i++) s.on[i] = on[i] && r.on[i];
+		return s;
+	}
+	Set<T, N> operator||(const Set<T, N>& r) {
+		Set<T, N> s;
+		for(int i=0; i<N; i++) s.on[i] = on[i] || r.on[i];
+		return s;
+	}
+	Set<T, N> operator^(const Set<T, N>& r) {
+		Set<T, N> s;
+		for(int i=0; i<N; i++) s.on[i] = on[i] ^ r.on[i];
+		return s;
+	}
 		
 protected:
 };
 
-template <typename T, int N>
-ostream& operator<<(ostream& o, const Set<T, N>& r) {
-	o << '{';
-	for(int i=0; i<N; i++) if(r.on[i]) o << r.arr[i] << ',';
-	o << "\b}";
-	return o;
-}
 
 int main()
 {
@@ -70,8 +92,12 @@ int main()
 	for(int i=0; i<5; i++) s1.insert(i);
 	Set<int, 10> s2;
 	for(int i=3; i<10; i++) s2.insert(i);
-	auto s3 = s1 & s2;
 	cout << s1 << " & " << s2 << " = " << (s1 & s2) << endl;
 	cout << s1 << " | " << s2 << " = " << (s1 | s2) << endl;
 	cout << s1 << " - " << s2 << " = " << (s1 - s2) << endl;
+	cout << "s1 = " << s1 << endl;
+	s1.remove(3);
+	cout << "deleted : " << !s1 << " then " << s1 << endl;
+	cout << "undelete : " << (!s1 || s1) << endl;
+	
 }
