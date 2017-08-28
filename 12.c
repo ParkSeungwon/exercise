a4b9f03859c87abd632a368f494432decc782d87
zezeon <zezeon@msn.com>
2017-08-29 07:43:05 +0900

    +

--- 12.c
+++ 12.c
@@ -1,26 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_LIST_SIZE 100
-
 typedef int element;
-typedef struct {
-	element list[MAX_LIST_SIZE];
-	int length;
-} ArrayListType;
+typedef struct Tree {
+	element data;
+	struct Tree *left, *right;
+} Tree;
 
-void clear(ArrayListType* l) {
-	l->length = 0;
+void show(Tree* node) {
+	if(!node) return;
+	show(node->left);
+	printf("%d ", node->data);
+	show(node->right);
 }
 
-void replace(ArrayListType* l, int pos, element item) {
-	l->list[pos] = item;
-}
-	
-element get_entry(ArrayListType* l, int pos) {
-	return l->list[pos];
+void free_mem(Tree* node) {
+	if(!node) return;
+	free_mem(node->left);
+	free_mem(node->right);
+	free(node);
 }
 
-int get_length(ArrayListType* l) {
-	return l->length;
+Tree* insert(Tree* node, element n) {
+	if(!node) {
+		node = (Tree*)malloc(sizeof(Tree));
+		node->data = n;
+		node->left = NULL;
+		node->right = NULL;
+	} else if(node->data > n) node->left = insert(node->left, n);
+	else node->right = insert(node->right, n);
+}
+
+Tree* delete(Tree* node) {
+	if(node->left && node->right) {
+		Tree* prev = p;
+		for(Tree* p = node->right; p; p = p->left) prev = p;
+		node->data = prev->data;
+		pprev->left = prev->right;
+		free(prev);
+	}
+}
+				}
+int main() {
+	Tree* root = insert(root, 8);
+	insert(root, 5);
+	insert(root, 7);
+	insert(root, 4);
+	insert(root, 12);
+	insert(root, 1);
+	insert(root, 9);
+	show(root);
+	free(root);
 }
